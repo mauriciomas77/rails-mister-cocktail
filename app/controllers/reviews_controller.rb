9e018bb2dbf90c3556b5af9516dab6a3834984da
Mauricio Mas <mauriciomastonon@gmail.com>
2020-06-23 20:23:13 -0300

    Add dotenv -Protect my data

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.delete
+    @cocktail = @review.cocktail
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+puts "Clearing database..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+puts "Creating new items.."
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+get_ingredients = open(ingredients_url).read
+get_cocktails = open(cocktails_url).read
+JSON.parse(get_ingredients)["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+JSON.parse(get_cocktails)["drinks"].each do |cocktail|
+  Cocktail.create!(
+    name: cocktail['strDrink'],
+    )
+end
+puts "Items created : #{Cocktail.count}"
